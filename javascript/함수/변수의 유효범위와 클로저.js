cc1ba591f730fc360d45a1eeae9d1b68351b4ee5
gj00770 <79184842+gj00770@users.noreply.github.com>
2022-04-25 21:24:55 +0900

    오늘공부

---
+++ javascript/함수/변수의 유효범위와 클로저.js
@@ -0,0 +1,78 @@
+// 클로저는 반환된 내부 함수가 자신이 선언됐을 때의 환경(lexical environment)인 스코프를 기억하여 자신이 선언됐을 때의 환경(스코프) 밖에서 호출되어도 그 환경(스코프)에 접근할 수 있는 함수를 말합니다. 
+
+// (간단히 말해서, 클로저는 자신이 생성될 때의 환경(lexical environment)을 기억하는 함수입니다.)
+
+ 
+
+// 자신을 포함하고있는 외부 함수보다 내부 함수가 더 오래 유지되는 경우, 외부 함수 밖에서 내부 함수가 호출되더라도 외부 함수의 지역 변수에 접근할 수 있는데 이러한 함수를 클로저라고 부른다.
+
+function makeWorker() {
+    let name = "Pete";
+  
+    return function() {
+      alert(name);
+    };
+  }
+  
+  let name = "John";
+  
+  // create a function
+  let work = makeWorker();
+  
+  // call it
+  work(); // what will it show?
+  //pete
+
+
+
+  function makeCounter() {
+    let count = 0;
+  
+    return function() {
+      return count++;
+    };
+  }
+  
+  let counter = makeCounter();
+  let counter2 = makeCounter();
+  
+  alert( counter() ); // 0
+  alert( counter() ); // 1
+  
+  alert( counter2() ); // 0
+  alert( counter2() ); // 1
+
+
+  function Counter() {
+    let count = 0;
+  
+    this.up = function() {
+      return ++count;
+    };
+  
+    this.down = function() {
+      return --count;
+    };
+  }
+  
+  let counter = new Counter();
+  
+  alert( counter.up() ); // 1
+  alert( counter.up() ); // 2
+  alert( counter.down() ); // 1
+
+
+
+
+
+
+
+
+let x = 1;
+
+function func() {
+  console.log(x); // ReferenceError: Cannot access 'x' before initialization
+  let x = 2;
+}
+
+func();
